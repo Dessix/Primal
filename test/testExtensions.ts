304131fe4b0a8a217b6b7d8127ce9d741eba79d8
Zoey <Dessix@Dessix.net>
2016-09-25 05:34:32 -0400

    Fixed accidental duplicate intersection test.

--- test/testExtensions.ts
+++ test/testExtensions.ts
@@ -13,30 +13,35 @@ describe("Extensions", () => {
       assert.deepEqual(Object.values({ x: 1, y: 2, 3: 2, 4: 2 }).sort(), [1, 2, 2, 2], "Duplicates are included");
     });
   });
-  describe("Intersection", () => {
-    it("Should provide intersection between two points", () => {
-      const output = RoomPosition.intersect({ x: 1, y: 1 }, 1, { x: 3, y: 3 }, 1);
-      assert.deepEqual(output, [{ x: 2, y: 2 }]);
+
+
+  describe("RoomPosition", () => {
+    describe("Intersection", () => {
+      it("Should provide intersection between two points", () => {
+        const output = RoomPosition.intersect({ x: 1, y: 1 }, 1, { x: 3, y: 3 }, 1);
+        assert.deepEqual(output, [{ x: 2, y: 2 }]);
+      });
+      it("Should provide nothing when no points can exist", () => {
+        const output = RoomPosition.intersect({ x: 1, y: 1 }, 1, { x: 5, y: 5 }, 2);
+        assert.lengthOf(output, 0);
+      });
+      it("Should provide intersection between multiple points", () => {
+        const output = RoomPosition.intersect({ x: 1, y: 1 }, 1, { x: 3, y: 3 }, 1, { x: 1, y: 3 }, 1);
+        assert.deepEqual(output, [{ x: 2, y: 2 }]);
+      });
     });
-    it("Should provide nothing when no points can exist", () => {
-      const output = RoomPosition.intersect({ x: 1, y: 1 }, 1, { x: 5, y: 5 }, 2);
-      assert.lengthOf(output, 0);
-    });
-    it("Should provide intersection between multiple points", () => {
-      const output = RoomPosition.intersect({ x: 1, y: 1 }, 1, { x: 3, y: 3 }, 1, );
-      assert.deepEqual(output, [{ x: 2, y: 2 }]);
-    });
-  });
-  describe("IntersectionArrayed", () => {
-    it("Should provide intersection between two points", () => {
-      const output = RoomPosition.intersectAll([[{ x: 1, y: 1 }, 1], [{ x: 3, y: 3 }, 1]]);
-      assert.deepEqual(output, [{ x: 2, y: 2 }]);
-    });
-    it("Should provide nothing when no points can exist", () => {
-      const output = RoomPosition.intersectAll([[{ x: 1, y: 1 }, 1], [{ x: 5, y: 5 }, 2]]);
-      assert.lengthOf(output, 0);
-      const outputRoomPos = RoomPosition.intersectAllRoomPos("TestChamber", [[{ x: 1, y: 1 }, 1], [{ x: 5, y: 5 }, 2]]);
-      assert.lengthOf(output, 0);
+
+    describe("IntersectionArrayed", () => {
+      it("Should provide intersection between two points", () => {
+        const output = RoomPosition.intersectAll([[{ x: 1, y: 1 }, 1], [{ x: 3, y: 3 }, 1]]);
+        assert.deepEqual(output, [{ x: 2, y: 2 }]);
+      });
+      it("Should provide nothing when no points can exist", () => {
+        const output = RoomPosition.intersectAll([[{ x: 1, y: 1 }, 1], [{ x: 5, y: 5 }, 2]]);
+        assert.lengthOf(output, 0);
+        const outputRoomPos = RoomPosition.intersectAllRoomPos("TestChamber", [[{ x: 1, y: 1 }, 1], [{ x: 5, y: 5 }, 2]]);
+        assert.lengthOf(output, 0);
+      });
     });
   });
 });
