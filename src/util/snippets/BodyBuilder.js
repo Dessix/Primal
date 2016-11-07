9d44e87c680640b816e1bb906697d88300a49757
Dessix <Dessix@Dessix.net>
2016-11-06 21:27:30 -0500

    Remove snippets from repository

--- src/util/snippets/BodyBuilder.js
+++
@@ -1,9 +0,0 @@
-StructureSpawn.prototype.generateUpgraderForCost = function (cost, controllerLevel = 7) {
-    let baseCreep = [WORK, CARRY];
-    let growParts = [WORK, CARRY];
-
-    let workParts = 15;
-    if (controllerLevel < 8) workParts = 50;
-    let opts = {useRoads: true, maxWork: workParts, maxCarry: 4};
-    return this.buildCreepBody(cost, baseCreep, growParts, opts);
-};
